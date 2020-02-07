5ffe0af28654f46806f1e6c60764f375279e2d51
Oliver Pope <ojaypopedev@gmail.com>
2020-02-07 00:24:03 +0000

    Project Cleanup
    Removed Lots of unused assets from the project folder,
    Organised folders and renamed certain things

---
+++ 2017 Project - Slide the Blocks/Assets/Scripts/HighScoreManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HighScoreManager : MonoBehaviour
+{
+
+    [SerializeField] private Text hiScoreText;
+
+    private int Best;
+
+    private readonly string highScore = "HIGH_SCORE";
+
+    [ContextMenu("Reset High Score")] public void ResetHighScore()
+    {
+        Best = 0;
+        PlayerPrefs.SetInt(highScore, 0);
+    }
+   
+
+    private void Awake()
+    {
+        Best = PlayerPrefs.GetInt(highScore);
+        
+
+    }
+
+    public void testScore(int score)
+    {
+        if(score > Best)
+        {
+            PlayerPrefs.SetInt(highScore, score);
+            Best = PlayerPrefs.GetInt(highScore);
+
+        }
+
+
+        hiScoreText.text = Best.ToString();
+
+    }
+
+}
---
+++ 2017 Project - Slide the Blocks/Assets/Scripts/ParentToGround.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class ParentToGround : MonoBehaviour {
+
+
+    private readonly string Ground = "Ground";
+    [SerializeField] GameObject ground;
+    [SerializeField] Vector3 raycastOffset;
+
+
+    private void Awake()
+    {
+     
+    }
+
+
+    private void Update()
+    {
+        RaycastHit hit;
+        Physics.Raycast(new Ray(transform.position, Vector3.down), out hit,3f);
+
+        
+      
+
+        if (hit.collider)
+        {
+            if (hit.collider.tag == Ground)
+            {
+                ground = hit.collider.gameObject;
+            }
+
+        }
+        else
+        {
+            ground = null;
+        }
+
+        transform.SetParent(ground.transform);
+
+        if (transform.parent)
+        {
+            Vector3 parentScale = transform.parent.lossyScale;
+            transform.localScale = new Vector3(1 / parentScale.x, 1 / parentScale.y, 1 / parentScale.z);
+
+        }
+        else
+        {
+            transform.localScale = Vector3.one;
+        }
+
+
+
+    }
+
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawRay(new Ray(transform.position+raycastOffset, Vector3.down));
+    }
+
+
+}
